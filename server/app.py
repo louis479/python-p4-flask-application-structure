41aa914275c12f041f4602deea9d96cfe1907d8c
louis-ngugi <louisngugi858@gmail.com>
2025-03-27 03:53:46 +0300

    created my HomePage

--- server/app.py
+++ server/app.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, app
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'  # Displays this text in the browser
+
+@app.route('/<string:username>')  # Accepts a username in the URL
+def user(username):
+    return f'<h1>Profile for {username}</h1>'  # Displays a custom message
